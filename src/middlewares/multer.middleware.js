d6852f4f890c710856c58cef8586458bd43c1686
Souravroy99 <souravroy200418@gmail.com>
2025-08-13 22:39:16 +0530

    Multer added

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer"
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/temp"); // folder where files will be saved
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname); // unique file name
+  }
+});
+
+// Create multer upload middleware
+export const upload = multer({ storage: storage });
